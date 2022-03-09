cee128ce720706bc267349484e194e271cd4b14d
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-03-09 18:25:00 +0100

    creazione schede, titoli e paragrafi.. con le rispettive classi e valori contrassegnati con "i"

--- script.js
+++ script.js
@@ -50,17 +50,56 @@ const team = [
     },
   ];
 
+/* let scheda =document.getElementById("my-scheda") */
+/* JSON.stringify(team); */
+    
+    for(let oggettiTeam in team){
+        console.log(team[oggettiTeam]) 
+        
+    }
 
-JSON.stringify(team);
+    /* for(let oggettiTeam in team){
+        document.getElementById("name").innerHTML=team[0].name
+        document.getElementById("role").innerHTML=team[0].role
+        document.getElementById("my-img").innerHTML=team[0].image
 
-for(let oggettiTeam in team){
-    console.log(team[oggettiTeam]) 
-}
+    } */
 
-for(i = 0; i <team.length; i++){
-     
-    document.getElementById("qui").innerHTML=team[i].name
-}
+    for(i = 0; i < team.length; i++){
+
+        /* creo un contenitore di tipo div */
+        let scheda = document.createElement("div")
+        /* gli aggiungo le varie calssi */
+        scheda.classList.add("col-4", "my-scheda", "m-2")
+        /* dico alla row nell'hatml di comprendere la scheda che ho screato (il div che ho creato) */
+        document.getElementById("ciccio").appendChild(scheda)
+        /* creo un h2 che farà da titolo per il nome */
+        let titleName = document.createElement("h2")
+        /* gli aggiungo le varie classi */
+        titleName.classList.add("my-title","text-center","my-3")
+        /* lo inserisco nel contenitore (la scheda che ho creato prima) */
+        scheda.appendChild(titleName)
+        /* GLI INSERISCO IL VALORE NAME CONTRASSEGNATO COME "I" DEI VARI OGGETTI CONTENUTI NELL'ARRAY TEAM */
+        titleName.innerHTML=team[i].name
+
+        /* creo un paragrafo per il ruolo */
+        let paragrafoRole= document.createElement("p")
+        /* gli do le classi */
+        paragrafoRole.classList.add("my-title","text-center")
+        /* inserisco il paragrafo nella scheda */
+        scheda.appendChild(paragrafoRole)
+        /* GLI DO IL VALORE IL VALORE ROLE CONTRASSEGNATO COME "I" NELL'ARRAY TEAM */
+        paragrafoRole.innerHTML=team[i].role
+
+        
+    }
 
 
-console.log(team[0].name,team[0].role)
+
+
+
+
+
+
+
+
